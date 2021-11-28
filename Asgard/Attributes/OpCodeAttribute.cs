36fd497d84c9c65893463b055d815d322eeb93b2
Richard Crawshaw <richardcrawshaw41@gmail.com>
2021-11-28 22:20:21 +1100

    Added t4 templates to generate OpCode classes, supporting abstract base classes and enumerations. Added OpCodeAttribute attribute class to allow the OpCode classes to be decorated with their meta-data. Also included is the data file that drives the code generation: cbus-opcodes.txt

---
+++ Asgard/Attributes/OpCodeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Asgard
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public partial class OpCodeAttribute : Attribute
+	{
+		/// <summary>
+		/// Gets and sets the op-code.
+		/// </summary>
+		public string Code { get; set; }
+
+		/// <summary>
+		/// Gets and sets the description.
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Gets and sets the group.
+		/// </summary>
+		public string Group { get; set; }
+
+		/// <summary>
+		/// Gets and sets the op-code name.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets and sets the op-code number.
+		/// </summary>
+		public byte Number { get; set; }
+
+		/// <summary>
+		/// Gets and sets the op-code priority.
+		/// </summary>
+		public int Priority { get; set; }
+	}
+}
---
+++ Asgard/Generated/Test/OpCodesTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asgard.Generated.Test
+{
+    class OpCodesTest
+    {
+        public static void Main()
+        {
+            // Working Directory:
+            // C:\Users\richa\source\repos\Asgard\Asgard\bin\Debug\net5.0
+            // Required file path:
+            // C:\Users\richa\source\repos\Asgard\Asgard\Generated\cbus-opcodes.txt
+            var loader = new Loader(@"..\..\..\Generated\cbus-opcodes.txt");
+            loader.Load();
+
+            Console.WriteLine($"EnumerationLines: {loader.EnumerationLines.Count}");
+            Console.WriteLine($"EnumerationNames: {loader.EnumerationNames.Count}");
+            Console.WriteLine($"FileCommentLines: {loader.FileCommentLines.Count}");
+            Console.WriteLine($"HistoryLines:     {loader.HistoryLines.Count}");
+            Console.WriteLine($"LicenceLines:     {loader.LicenceLines.Count}");
+            Console.WriteLine($"OpCodeLines:      {loader.OpCodeLines.Count}");
+            Console.WriteLine($"OpCodeNumbers:    {loader.OpCodeNumbers.Count}");
+            Console.WriteLine($"PropertyLines:    {loader.PropertyLines.Count}");
+
+            var builder = new Builder(loader);
+            builder.Build();
+
+            foreach(var opCodeBlock in builder.OpCodeBlocks)
+            {
+                Console.WriteLine("{0:X2} V\t{1}\t{2}\t{3}\t\"{4}\"", opCodeBlock.Value, 
+                    opCodeBlock.Code, opCodeBlock.Priority, opCodeBlock.Group, opCodeBlock.Name);
+
+                Console.WriteLine("{0:X2} D\t{1}", opCodeBlock.Value, opCodeBlock.Description);
+                
+                foreach (var comment in opCodeBlock.Comments)
+                    Console.WriteLine("{0:X2} C\t{1}", opCodeBlock.Value, comment);
+                
+                foreach (var property in opCodeBlock.Properties)
+                    Console.WriteLine("{0:X2} P\t{1}\t{2}\t{3}", opCodeBlock.Value, 
+                        property.Source, property.Name, property.DataType);
+
+                Console.WriteLine("{0:X2} S\t{1}", opCodeBlock.Value, opCodeBlock.ToStringText);
+            }
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
