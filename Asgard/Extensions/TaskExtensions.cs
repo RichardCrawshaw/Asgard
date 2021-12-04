0c157d76648b0b25b30e064a19b93b318af6eaa4
James Thorpe <james@pawsforthorpe.co.uk>
2021-12-04 12:00:43 +0000

    initial basic comms classes, unit tests at the gridconnect level

---
+++ Asgard/Extensions/TaskExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asgard
+{
+    public static class TaskExtensions
+    {
+        public static void AsgardFireAndForget(this Task task)
+        {
+            if (!task.IsCompleted || task.IsFaulted)
+            {
+                _ = ForgetAwaited(task);
+            }
+
+            static async Task ForgetAwaited(Task task)
+            {
+                try
+                {
+                    await task.ConfigureAwait(false);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
