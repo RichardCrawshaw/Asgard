284a318386d7e0426148d5234192f56676e63eaa
Richard Crawshaw <richardcrawshaw41@gmail.com>
2022-01-25 22:45:55 +1100

    Fixed failing multiple messages in flight test.

--- Asgard/Communications/Classes/MessageManager.cs
+++ Asgard/Communications/Classes/MessageManager.cs
@@ -30,7 +30,8 @@ namespace Asgard.Communications
         /// <param name="filterResponse">An optional filter callback to further process the replies to ensure you get the right one.</param>
         /// <returns>The first message of the given type that passes the filterResponse.</returns>
         public Task<T> SendMessageWaitForReply<T>(ICbusOpCode msg, Func<T, bool> filterResponse = null)
-            where T : ICbusOpCode => SendMessageWaitForReply(msg, DefaultTimeout, filterResponse);
+            where T : ICbusOpCode => 
+            SendMessageWaitForReply(msg, DefaultTimeout, filterResponse);
 
         /// <summary>
         /// Sends a message and awaits a given reply, or times out.
@@ -41,7 +42,8 @@ namespace Asgard.Communications
         /// <param name="filterResponse">An optional filter callback to further process the replies to ensure you get the right one.</param>
         /// <returns>The first message of the given type that passes the filterResponse.</returns>
         public async Task<T> SendMessageWaitForReply<T>(ICbusOpCode msg, TimeSpan timeout, Func<T, bool> filterResponse = null)
-            where T : ICbusOpCode => (await SendMessageWaitForReplies(msg, timeout, 1, filterResponse)).First();
+            where T : ICbusOpCode => 
+            (await SendMessageWaitForReplies(msg, timeout, 1, filterResponse)).First();
 
         /// <summary>
         /// Sends a message and waits for replies, or times out.
@@ -52,7 +54,8 @@ namespace Asgard.Communications
         /// <param name="filterResponses">An optional filter callback to further process the replies to ensure you get the right ones.</param>
         /// <returns>The received responses.</returns>
         public Task<IEnumerable<T>> SendMessageWaitForReplies<T>(ICbusOpCode msg, int expected = 0, Func<T, bool> filterResponses = null)
-            where T : ICbusOpCode => SendMessageWaitForReplies(msg, DefaultTimeout, expected, filterResponses);
+            where T : ICbusOpCode => 
+            SendMessageWaitForReplies(msg, DefaultTimeout, expected, filterResponses);
 
         /// <summary>
         /// Sends a message and awaits a given reply, or times out.
@@ -60,7 +63,8 @@ namespace Asgard.Communications
         /// <typeparam name="T">The type of message to send, and use Asgard to filter and return known reply messages for.</typeparam>
         /// <param name="msg">The message to send.</param>
         public async Task<IReplyTo<T>> SendMessageWaitForReply<T>(T msg)
-            where T : ICbusOpCode => (await SendMessageWaitForReplies<T>(msg, DefaultTimeout, 1)).First();
+            where T : ICbusOpCode => 
+            (await SendMessageWaitForReplies<T>(msg, DefaultTimeout, 1)).First();
 
         /// <summary>
         /// Sends a message and awaits a given reply, or times out.
@@ -69,7 +73,8 @@ namespace Asgard.Communications
         /// <param name="msg">The message to send.</param>
         /// <param name="timeout">The amount of time to wait for a response.</param>
         public async Task<IReplyTo<T>> SendMessageWaitForReply<T>(T msg, TimeSpan timeout)
-            where T : ICbusOpCode => (await SendMessageWaitForReplies<T>(msg, timeout, 1)).First();
+            where T : ICbusOpCode => 
+            (await SendMessageWaitForReplies<T>(msg, timeout, 1)).First();
 
         /// <summary>
         /// Send a message and waits for replies, or times out.
@@ -78,7 +83,8 @@ namespace Asgard.Communications
         /// <param name="message">The message to send.</param>
         /// <param name="expected">The number of expected responses. Times out if this number is not received. Specify 0 for an unknown number of responses. Returns immediately when this number of responses have been received.</param>
         /// <returns>The received responses.</returns>
-        public Task<IEnumerable<IReplyTo<T>>> SendMessageWaitForReplies<T>(T message, int expected = 0) where T : ICbusOpCode =>
+        public Task<IEnumerable<IReplyTo<T>>> SendMessageWaitForReplies<T>(T message, int expected = 0)
+            where T : ICbusOpCode =>
             SendMessageWaitForReplies(message, DefaultTimeout, expected);
 
         /// <summary>
@@ -102,7 +108,11 @@ namespace Asgard.Communications
         /// <param name="expected">The number of expected responses. Times out if this number is not received. Specify 0 for an unknown number of responses. Returns immediately when this number of responses have been received.</param>
         /// <param name="filterResponses">An optional filter callback to further process the replies to ensure you get the right ones.</param>
         /// <returns>The received responses.</returns>
-        public async Task<IEnumerable<T>> SendMessageWaitForReplies<T>(ICbusOpCode msg, TimeSpan timeout, int expected = 0, Func<T, bool> filterResponses = null) where T : ICbusOpCode
+        public async Task<IEnumerable<T>> SendMessageWaitForReplies<T>(ICbusOpCode msg,
+                                                                       TimeSpan timeout,
+                                                                       int expected = 0,
+                                                                       Func<T, bool> filterResponses = null)
+            where T : ICbusOpCode
         {
             this.logger?.LogTrace($"Sending message of type {0}, awaiting {1} replies with a timeout of {2}",
                 msg.GetType().Name, typeof(T).Name, expected);
@@ -146,12 +156,10 @@ namespace Asgard.Communications
             }
 
             this.messenger.MessageReceived += AwaitResponse;
-            var sent = await this.messenger.SendMessage(msg.Message);
+            await this.messenger.SendMessage(msg.Message);
 
             try
             {
-                if (!sent) return responses;
-
                 var all = await tcs.Task;
                 if (!all)
                 {
