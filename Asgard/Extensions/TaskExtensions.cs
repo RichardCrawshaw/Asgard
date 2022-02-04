6e8aa8fe06767227e6707cca89b1767706c0a159
Richard Crawshaw <richardcrawshaw41@gmail.com>
2022-02-04 22:57:27 +1100

    Added extra FireAndForget extension methods.

--- Asgard/Extensions/TaskExtensions.cs
+++ Asgard/Extensions/TaskExtensions.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Asgard
 {
@@ -21,10 +20,51 @@ namespace Asgard
                 {
                     await task.ConfigureAwait(false);
                 }
-                catch
-                {
+                catch { }
+            }
+        }
 
+        public static void AsgardFireAndForget<TException>(this Task task, ILogger? logger, string message)
+            where TException : Exception
+        {
+            if (!task.IsCompleted || task.IsFaulted)
+            {
+                _ = ForgetAwaited(task, logger, message);
+            }
+
+            static async Task ForgetAwaited(Task task, ILogger? logger, string message)
+            {
+                try
+                {
+                    await task.ConfigureAwait(false);
                 }
+                catch (TException ex)
+                {
+                    logger?.LogWarning(ex, message);
+                }
+                catch { }
+            }
+        }
+
+        public static void AsgardFireAndForget<TException>(this Task task, ConcurrentQueue<Exception> queue)
+            where TException: Exception
+        {
+            if (!task.IsCompleted || task.IsFaulted)
+            {
+                _ = ForgetAwaited(task, queue);
+            }
+
+            static async Task ForgetAwaited(Task task, ConcurrentQueue<Exception> queue)
+            {
+                try
+                {
+                    await task.ConfigureAwait(false);
+                }
+                catch (TException ex)
+                {
+                    queue.Enqueue(ex);
+                }
+                catch { }
             }
         }
     }
